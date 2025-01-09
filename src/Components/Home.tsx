462c5327f59897bbad57a7d5c1ee5ba02138a8eb
kellysand <kellyxsand@gmail.com>
2025-01-09 13:17:18 +0200

    home ui done

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -4,22 +4,29 @@ import bigcvimg from "../assets/image/Bigcvimg.png";
 import './Home.css'
 function Home() {
   return (
+    
     <div className=" pt-20  px-12 flex ">
         <section className="flex flex-col gap-10 flex-1">
             <div>
-                <h1  className=" font-ibmmono text-4xl">Create a Professional <><br /></>
-                Resume with <span className="text-red-600">Resume Creator</span></h1>
-                <p className="pt-9 ibm-plex-mono-medium-italic ">A CV that gets the Job DONE <span className="text-red-600">100%</span></p>
+                <h1  className=" font-ibmmono text-4xl font-bold">Create a Professional 
+                 Resume with <span className="text-red-600">Rezume-TooL</span></h1>
+                <p className=" ibm-plex-mono-medium-italic ">A Resume that gets the Job DONE <span className="text-red-600">100%</span></p>
+               <br /> <p className=" font-sans text-lg w-5/6">Create polished, professional resumes in just a few clicks! Our powerful resume builder offers intuitive features and customizable templates tailored to your needs.
+                   Stand out from the competition, impress recruiters, and land your dream job faster. Build your resume today—because first impressions matter!</p>
             </div>
-            <div className=""><button className=" h-16 w-36  rounded-lg bg-sky-500 shadow-lg shadow-black ">Create CV</button></div>
-            <picture className='flex justify-center gap-16 pl-16 '>
-            <img className='cv  shadow-md  ' src={smallCvimg} alt="" />
-            <img className ='cv  shadow-md 'src={smallCvimg2} alt="" />
-            </picture>
+            <div className="flex justify-evenly"><button className=" h-16 w-36  rounded-lg bg-sky-500 shadow-lg shadow-black ibm-plex-mono-regular-italic  ">Create CV</button>
+              <img className=' h-64 w-44 shadow-2xl  shadow-zinc-500 rounded-md hover:shadow-sky-300   ' src={smallCvimg} alt="" /> 
+             {/* <img className ='cv  shadow-md 'src={smallCvimg2} alt="" />  */}
+            </div>
+            
         </section>
-        <section className='flex-1 flex justify-center'>
-          <img  className =' shadow-2xl  ml-24  'src={bigcvimg} alt="" />
-                 </section>
+        <section className='flex-1 justify-items-center'>
+          <div className='   rounded-full border-2 border-black w-full justify-items-center'>
+          <div className='   rounded-full border-2 border-black  w-10/12 h-4/6 justify-items-center  '>
+          <img  className =' shadow-2xl   'src={bigcvimg} alt="" />
+          </div>
+          </div>
+        </section>
 
 
     </div>
