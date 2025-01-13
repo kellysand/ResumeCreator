90957437554733e1afb35b68a6816f89acf1c378
kellysand <kellyxsand@gmail.com>
2025-01-13 08:54:50 +0200

    Added React router to link the pages to each other. Added prop lifting to shear data to complonents

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
 import NavBar from './Components/NavBar'
 import './App.css'
 import Home from './Components/Home'
-
+import Template from './Components/Template'
+import CreateCv from './Components/CreateCv'
+import { useState } from 'react'
 function App() {
- 
+  const [chosenTemp, setChosenTemp] = useState<React.ReactNode | null>(null);
   return (
     <>
+    <Router>
       <NavBar/>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+         <Route path='/templates' element={<Template ChosenTemp={setChosenTemp}/>}/>
+        <Route path='/create' element={<CreateCv ChosenTemp={chosenTemp}/>}/>
+      </Routes>
+   </Router>
 
     </>
   )
